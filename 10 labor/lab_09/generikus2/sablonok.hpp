4f6e97554ad724b573e53e31fd3b4d16a60dc59a
Krisztián Hőgyes <hogyesk@edu.bme.hu>
2021-04-12 11:35:45 +0200

    Fine

---
+++ 10 labor/lab_09/generikus2/sablonok.hpp
@@ -0,0 +1,52 @@
+#ifndef SABLONOK_HPP_INCLUDED
+#define SABLONOK_HPP_INCLUDED
+
+#define ELKESZULT 7
+
+template <typename Iter>
+void printEach(Iter first, Iter last, std::ostream& os = std::cout, const char * sep = ", ") {
+	while (true) {
+		os << *first++;
+		if (first == last) {
+			os << std::endl;
+			break;
+		}
+		else {
+			os << sep;
+		}
+	}
+}
+
+template <typename Iter>
+int szamol_ha_negativ(Iter first, Iter last) {
+	int db = 0;
+	while (first != last) {
+		if (*first++ < 0) {
+			db++;
+		}
+	}
+	return db;
+}
+
+template <typename T>
+class nagyobb_mint {
+	T t;
+public:
+	nagyobb_mint(T x) :t(x) {}
+	bool operator()(T x) {
+		return x > t;
+	}
+};
+
+template <typename Iter, typename Pred>
+int szamol_ha(Iter first, Iter last, Pred pred) {
+	int db = 0;
+	while (first != last) {
+		if (pred(*first++)) {
+			db++;
+		}
+	}
+	return db;
+}
+
+#endif // SABLONOK_HPP_INCLUDED
